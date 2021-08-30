a1b22d3ceadb60991643d6da093caa5cf36d2900
Pascal Eberlein <pascal@eberlein.io>
2021-08-30 17:57:21 +0200

    add grafana_setup.sh

---
+++ grafana_setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+wget -q -O - https://packages.grafana.com/gpg.key | sudo apt-key add -
+echo "deb https://packages.grafana.com/oss/deb stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
+sudo apt-get update
+sudo apt-get install -y grafana
+sudo /bin/systemctl enable grafana-server
+sudo /bin/systemctl start grafana-server
